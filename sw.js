ac27cc675ac871f03ee341f9e054210b75061f04
davidz91800 <davidz91800@gmail.com>
2026-03-05 17:18:00 +0100

    Create Debrief Primer PWA with no-cache service worker

---
+++ sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((key) => caches.delete(key)));
+      await self.clients.claim();
+    })()
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' }).catch(
+      () =>
+        new Response('', {
+          status: 503,
+          statusText: 'Offline'
+        })
+    )
+  );
+});
